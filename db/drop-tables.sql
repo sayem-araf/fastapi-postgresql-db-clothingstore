d842013009a6b5488fd9be28ac341c860ae335cb
Sayem Araf <139066946+sayem-araf@users.noreply.github.com>
2026-02-11 00:42:05 +0200

    db refreshed

--- db/drop-tables.sql
+++
@@ -1,9 +0,0 @@
--- ============================
--- DROP ALL TABLES
--- ============================
-
-DROP TABLE IF EXISTS order_items CASCADE;
-DROP TABLE IF EXISTS orders CASCADE;
-DROP TABLE IF EXISTS products CASCADE;
-DROP TABLE IF EXISTS categories CASCADE;
-DROP TABLE IF EXISTS customers CASCADE;
